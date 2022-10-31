fe8ba83fa58f4a7cbe454f2c7cf429c39e7c9bb0
Adesh Nayak <adeshnayak6@gmail.com>
2022-10-31 08:57:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ToggleBar from './Components/ToggleBar';
+import Options from './Components/Options';
+
+function App() {
+  return (
+    <div className='main-leg'>
+      <ToggleBar  />
+      <Options />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ToggleBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../Styles/ToggleBar.css';
+
+const ToggleBar = () => {
+    return (
+        <div className="main-container">
+            <label className="toggler_label">Select Segments</label>
+            <div className="toggle-container">
+                <input className="radio_input" type="radio" name="toggle_option" id="toggle-option-one" />
+                <label className="radio_label" htmlFor="toggle-option-one">Features</label>
+                <input className="radio_input" type="radio" name="toggle_option" id="toggle-option-two" />
+                <label className="radio_label" htmlFor="toggle-option-two">Options</label>
+            </div>
+        </div>
+    )
+}
+
+
+export default ToggleBar;
